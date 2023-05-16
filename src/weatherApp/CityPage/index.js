b6d7ad8534c0a3f977c7a74dcac28c48234bc9b5
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2023-05-16 12:38:07 +0200

    added new backgrounds based on current weather condition

--- src/weatherApp/CityPage/index.js
+++ src/weatherApp/CityPage/index.js
@@ -10,12 +10,20 @@ import rainbackground from "./BackgroundImages/rainbackground.jpg";
 import clearbackground from "./BackgroundImages/clearbackground.jpg";
 import cloudsbackground from "./BackgroundImages/cloudsbackground.jpg";
 import snowbackground from "./BackgroundImages/snowbackground.jpg";
+import brokenclouds from "./BackgroundImages/brokenclouds.jpg";
+import scatteredclouds from "./BackgroundImages/scatteredclouds.jpg";
+import fewclouds from "./BackgroundImages/fewclouds.jpg";
+import thunderstorm from "./BackgroundImages/thunderstorm.jpg";
 
 const CityPage = () => {
   const { weatherData } = useFindCitiesByCoords();
 
   const selectBackground = () => {
     const currentCondition = weatherData.current.weather[0].main;
+    const currentConditionDescription =
+      weatherData.current.weather[0].description;
+
+    console.log(currentConditionDescription);
 
     switch (currentCondition) {
       case "Rain":
@@ -24,12 +32,23 @@ const CityPage = () => {
       case "Clear":
         document.body.style.backgroundImage = `url('${clearbackground}')`;
         break;
+      case "Fog":
       case "Clouds":
-      case "Mist":
-        document.body.style.backgroundImage = `url('${cloudsbackground}')`;
+        if (currentConditionDescription === "overcast clouds") {
+          return (document.body.style.backgroundImage = `url('${cloudsbackground}')`);
+        } else if (currentConditionDescription === "broken clouds") {
+          return (document.body.style.backgroundImage = `url('${brokenclouds}')`);
+        } else if (currentConditionDescription === "scattered clouds") {
+          return (document.body.style.backgroundImage = `url('${scatteredclouds}')`);
+        } else if (currentConditionDescription === "few clouds") {
+          return (document.body.style.backgroundImage = `url('${fewclouds}')`);
+        }
         break;
       case "Snow":
         document.body.style.backgroundImage = `url('${snowbackground}')`;
+        break;
+      case "Thunderstorm":
+        document.body.style.backgroundImage = `url('${thunderstorm}')`;
         break;
       default:
         document.body.style.backgroundColor = "lightblue";
